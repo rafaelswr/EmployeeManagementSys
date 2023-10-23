bd385549fb889621ed5a61378956a756d6b2ecbd
rafaelswr <rafaels.borges91@gmail.com>
2023-10-23 17:10:07 +0100

    Initial

---
+++ EmployeeSystem/backend/src/test/java/system/employee/employeesystem/EmployeeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package system.employee.employeesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
