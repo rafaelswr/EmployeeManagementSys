4b56480d1ec397051dc1715069981e1121857afd
rafaelswr <rafaels.borges91@gmail.com>
2023-10-26 23:38:14 +0100

    Config axios - http request to backend, and remove some unused stuff
    Config axios - http request to backend, and remove some unused stuff

---
+++ EmployeeSystem/frontend/src/api/axiosConfig.js
@@ -0,0 +1,7 @@
+
+import axios from "axios";
+
+export default axios.create({
+    baseURL:"https://6d24-2001-8a0-f76b-1400-de1f-a414-b95-6c7f.ngrok-free.app", 
+    headers:{"ngrok-skip-browser-warning":"true"}
+})
