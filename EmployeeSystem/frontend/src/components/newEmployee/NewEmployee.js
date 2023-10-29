4a0e2ea1f46a6029db752a1367c18c7c4624a292
rafaelswr <rafaels.borges91@gmail.com>
2023-10-29 16:43:31 +0000

    New Employee Page with Form(react-bootstrap) to add new instance
    New Employee Page with Form(react-bootstrap) to add new instance

---
+++ EmployeeSystem/frontend/src/components/newEmployee/NewEmployee.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./NewEmployee.css";
+import { Form, Button} from 'react-bootstrap';
+
+export const NewEmployee = () => {
+
+  return (
+      <Form>
+        
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email"  placeholder="ex: a@gmail.com" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+    
+    </Form>
+  )
+}
+
+
