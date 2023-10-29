eb6608289bda22657fde80fca4055fdc6ec7fecc
rafaelswr <rafaels.borges91@gmail.com>
2023-10-29 16:45:23 +0000

    page updateEmployee when select edit button in HomePage
    page updateEmployee when select edit button in HomePage

---
+++ EmployeeSystem/frontend/src/components/updateEmployee/UpdateEmployee.js
@@ -0,0 +1,47 @@
+import React,{useEffect, useRef} from 'react'
+import { useParams } from 'react-router-dom';
+import {Form, Button} from "react-bootstrap";
+import api from "../../api/axiosConfig";
+
+const UpdateEmployee = ({getSingleEmployee, employee, setEmployee}) => {
+
+    const {employeeID} = useParams(); 
+    const emailRef = useRef(); 
+
+    useEffect(()=>{
+        if(employeeID!=null){
+            getSingleEmployee(employeeID);
+        }
+        return ()=>{setEmployee({})}
+    },[employeeID]);
+    
+    const onClickSubmit = async (e)=>{
+     
+
+    }
+
+  
+    return (
+
+        <Form onSubmit={onClickSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" ref={emailRef} defaultValue={employee.email} placeholder="Enter email" />
+            </Form.Group>
+                <Button variant="primary" type="submit">
+                Submit
+            </Button>
+      </Form>
+  
+  
+   )
+}
+
+export default UpdateEmployee; 
+
+/**
+ *    <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+ */
