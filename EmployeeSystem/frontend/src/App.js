4e60845988fa065ed2cf42e75bdef66c0f92d268
rafaelswr <rafaels.borges91@gmail.com>
2023-12-30 01:26:49 +0000

    Update SSE
    Update SSE

--- EmployeeSystem/frontend/src/App.js
+++ EmployeeSystem/frontend/src/App.js
@@ -8,16 +8,39 @@ import Layout from './components/Layout';
 import api from "./api/axiosConfig.js";
 import UpdateEmployee from './components/updateEmployee/UpdateEmployee';
 
+//import { Stomp } from '@stomp/stompjs';
+//import SockJS from 'sockjs-client';
 
 function App() {
+ 
+const [employees, setEmployees] = useState([]);
+const [theads, setTHeads] = useState([]);
+const [employee, setEmployee] = useState({});
 
-    const [employees, setEmployees] = useState([]);
-    const [theads, setTHeads] = useState([]);
-    const [employee, setEmployee] = useState({});
+//WEBSOCKETS with Spring
+/* 
+useEffect(() => {
 
-    
-    //get All 
-    const getEmployees = async ()=>{
+  const client = Stomp.over(function(){
+    return new SockJS("http://localhost:8080/employees");
+  });
+  client.connect({}, () => {
+    console.log('Connected');
+    client.subscribe('/topic/employees', (message) => {
+      const updatedEmployees = JSON.parse(message.body);
+      setEmployees(updatedEmployees);
+    });
+  });
+   return () => {
+    client.deactivate(); // Use deactivate em vez de disconnect
+  };
+}, []);
+
+
+*/
+
+//get All 
+     const getEmployees = async ()=>{
       try {
         const response = await api.get("/employees");
         setEmployees(response.data);
@@ -28,12 +51,25 @@ function App() {
 
     useEffect(() => {
       getEmployees(); 
-    },[employees]);
+    },[]);
 
+
+    useEffect(()=>{
+      const eventSource = new EventSource('http://localhost:8080/sse-employees/stream');
+
+        eventSource.onmessage = (event) => {
+            const employeeData = JSON.parse(event.data);
+            getEmployees(); 
+        };
+
+        return () => {
+            eventSource.close();
+        };
+    },[]);
 
     useEffect(() => {
         const newTheads = [];
-
+      
         employees.forEach((employee) => {
           for (const key in employee) {
             if (!newTheads.includes(key)) {
@@ -43,14 +79,13 @@ function App() {
         });
       
         setTHeads(newTheads);
-      
+          
       }, [employees]);
 
     const getSingleEmployee = async (employeeId)=> {
         try {
           const response = await api.get(`/employees/${employeeId}`);
           setEmployee(response.data);
-          
         } catch (error) {
           console.log("Error get single employee");
         }
@@ -61,10 +96,11 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Layout/>}>
-          <Route path='/employees' element={<Home employees={employees} getSingleEmployee={getSingleEmployee} theads={theads}></Home>}/>
-          <Route path='/new_employee' element={<NewEmployee></NewEmployee>}></Route>
-          <Route path='/employees/:employeeID' element={<UpdateEmployee setEmployee={setEmployee} getSingleEmployee={getSingleEmployee} employee={employee}></UpdateEmployee>}></Route>
-        </Route>
+            <Route path='/employees' element={<Home employees={employees} getSingleEmployee={getSingleEmployee} theads={theads}></Home>}/>
+            <Route path='/new_employee' element={<NewEmployee></NewEmployee>}></Route>
+            <Route path='/employees/:employeeID' element={<UpdateEmployee setEmployee={setEmployee} getSingleEmployee={getSingleEmployee} employee={employee}></UpdateEmployee>}></Route>
+
+          </Route>
       </Routes>
     </div>
   );
--- EmployeeSystem/frontend/src/components/header/Header.js
+++ EmployeeSystem/frontend/src/components/header/Header.js
@@ -7,12 +7,12 @@ import {NavLink }from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" >
-        <Container className="Black">
-            <Navbar.Brand>
-                <h1>Employee Management System</h1>
+    <Navbar bg="dark" variant="dark" expand="lg">
+        <Container className="Black" style={{ margin:"0 auto", maxWidth:1280}}>
+            <Navbar.Brand >
+                <h2>Employee Management System</h2>
             </Navbar.Brand>
-            <Nav className="me-right my-2 my-lg-0">
+            <Nav className="me-right  my-lg-0">
                 <NavLink className="nav-link" to="/employees">Home</NavLink>
                 <NavLink className="nav-link" to="/new_employee">New Employee</NavLink>
           </Nav>
